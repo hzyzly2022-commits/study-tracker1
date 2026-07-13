effd57b4af6b18ad9ab9ac59e8b7757a2e62f263
hzyzly2022-commits <hzyzly2022@163.com>
2026-07-13 18:13:24 +0800

    更新了软件问候语

--- main.py
+++ main.py
@@ -1,4 +1,27 @@
-study_content = input("请输入今天学习内容：")
-study_time = input("请输入今天学习时间：")
-print("今天学习内容：", study_content)
-print("今天学习时间：", study_time, "继续加油")
+def show_menu():
+    print("""
+========================
+      Study Tracker
+========================
+欢迎使用Study Tracker
+1. 添加学习记录
+2. 查看学习记录
+3. 退出
+
+请选择：""", end="")
+
+
+show_menu()
+choice = input()
+
+if choice == "1":
+    study_content = input("请输入今天学习内容：")
+    study_time = input("请输入今天学习时间：")
+    print("今天学习内容：", study_content)
+    print("今天学习时间：", study_time, "继续加油")
+elif choice == "2":
+    print("查看学习记录")
+elif choice == "3":
+    print("已退出")
+else:
+    print("无效选项，请重新输入。")
